d0d77b34218ef63f443541932ec9cdda996b1853
Jarkyman1 <jackiehartvig@hotmail.com>
2017-02-20 13:51:04 +0100

    Addet stakviews

--- Pokedex3/PokemonDetailVC.swift
+++ Pokedex3/PokemonDetailVC.swift
@@ -13,6 +13,17 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
 
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var DefenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     
     override func viewDidLoad() {
@@ -24,5 +35,11 @@ class PokemonDetailVC: UIViewController {
         
         
     }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
